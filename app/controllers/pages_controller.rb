3b81eb5feaff7f2a65cc8030d6bdaea501e9cad1
Daniel <danielnguyensr@gmail.com>
2017-03-18 00:01:43 -0700

    Add migration, model, controller for photographers

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,9 @@ class PagesController < ApplicationController
   def index
   end
 
+  def studio
+  end
+
   def gaming
     @streamers = Streamer.all
   end
---
+++ spec/factories/photographers.rb
@@ -0,0 +1,12 @@
+FactoryGirl.define do
+  factory :photographer do
+    handle "MyString"
+    profile "MyText"
+    avatar "MyString"
+    flickr "MyString"
+    facebook "MyString"
+    tistory "MyString"
+    twitter "MyString"
+    youtube "MyString"
+  end
+end
