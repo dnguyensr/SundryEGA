e7ce618e9b8485b0ede34c52595293d220087884
Daniel <danielnguyensr@gmail.com>
2017-03-17 22:54:22 -0700

    Add streamers routes, views, styling

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :streamers
   root 'pages#index'
   PagesController.action_methods.each do |action|
       get "/#{action}", to: "pages##{action}", as: "#{action}_page"
