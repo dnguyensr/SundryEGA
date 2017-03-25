6104e731220920fe1284def9119808efefe30930
Daniel <danielnguyensr@gmail.com>
2017-03-24 19:07:56 -0700

    Modify models to make association optional

--- app/models/streamer.rb
+++ app/models/streamer.rb
@@ -1,5 +1,5 @@
 class Streamer < ApplicationRecord
   validates :handle, :games, :twitch, presence: true
   validates :handle, uniqueness: true
-  belongs_to :user
+  belongs_to :user, optional: true
 end
