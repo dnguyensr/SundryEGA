1f2d3eb9fd46976c3a4f880181100099cf79cd60
Daniel <danielnguyensr@gmail.com>
2017-04-06 22:33:43 -0700

    Add photographer components, update view

--- app/assets/javascripts/components/Photographer.js.jsx
+++ app/assets/javascripts/components/Photographer.js.jsx
@@ -1,23 +1,29 @@
 var Photographer = React.createClass({
-    render: function () {
+    render () {
         return (
-        <div className="col-xs-12 col-sm-6 gaming-profile">
-          <div className="row">
-            <div className="col-xs-3">
-              avatar
-              {/*<%= image_tag(photographer.avatar, className:"img-responsive")%>*/}
-            </div>
-            <div className="col-xs-9">
-              handle
-              profile
-              {/*<h1 className="gamertag"><%= photographer.handle %></h1>*/}
-              {/*<p><%= photographer.profile %></p>*/}
-              <div className="sns">
-                sns
-                {/*<%= link_to image_tag("icons/icon_youtube", alt:"youtube icon", className:"img-responsive sns-icon"), photographer.youtube if photographer.youtube.present? %>
-                <%= link_to image_tag("icons/icon_twitter", alt:"twitter icon", className:"img-responsive sns-icon"), photographer.twitter if photographer.twitter.present? %>
-                <%= link_to image_tag("icons/icon_instagram", alt:"instagram icon", className:"img-responsive sns-icon"), photographer.instagram  if photographer.instagram.present? %>*/}
-              </div>
+        <div className="row">
+          <div className="col-xs-3">
+            <img src={ this.props.photographer.avatar } className="img-responsive" />
+          </div>
+          <div className="col-xs-9">
+            <h1 className="gamertag">{ this.props.photographer.handle }</h1>
+            <p>{ this.props.photographer.profile }</p>
+            <div className="sns">
+              { this.props.photographer.youtube != null &&
+                <a href={ this.props.photographer.youtube }>
+                  <img src="assets/icons/icon_youtube" alt="youtube icon" className="img-responsive sns-icon" />
+                </a>
+              }
+              { this.props.photographer.twitter != null &&
+                <a href={ this.props.photographer.twitter }>
+                  <img src="assets/icons/icon_twitter" alt="twitter icon" className="img-responsive sns-icon" />
+                </a>
+              }
+              { this.props.photographer.instagram != null &&
+                <a href={ this.props.photographer.instagram }>
+                  <img src="assets/icons/icon_instagram" alt="instagram icon" className="img-responsive sns-icon" />
+                </a>
+              }
             </div>
           </div>
         </div>
